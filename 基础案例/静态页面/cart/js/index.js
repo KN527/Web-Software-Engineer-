84293d890cbae741f7f077102fa132b316b2f7a3
KN527 <nnkk527@outlook.com>
2018-12-23 00:02:39 +0800

    Create Cart Demo

---
+++ 基础案例/静态页面/cart/js/index.js
@@ -0,0 +1,8 @@
+$(function () {
+	$('.container').fullpage({
+		// 配置参数
+		sectionsColor: ["#fadd67" ,"#84A2D4" ,"#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+		navigation: true,
+		verticalCentered: false
+	});
+});
