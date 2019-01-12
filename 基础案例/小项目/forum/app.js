e19a4dd304259c33354c5783a3a9ac8c92b49da9
Eddie <37278660+KN527@users.noreply.github.com>
2019-01-12 15:04:06 +0800

    创建项目静态页面

--- 基础案例/小项目/forum/app.js
+++ 基础案例/小项目/forum/app.js
@@ -5,3 +5,14 @@ var app = express()
 
 app.use('/public', express.static(path.join(__dirname, './public/')))
 app.use('/node_modules', express.static(path.join(__dirname, './node_modules/')))
+
+app.engine('html', require('express-art-template'))
+app.set('views', path.join(__dirname, './views/'))// 默认的就是views目录
+
+app.get('', function (req, res) {
+	res.send('hello')
+})
+
+app.listen(3000, function () {
+	console.log('Server running on 3000 ...')
+})
