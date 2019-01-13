24b9f703f33bcd5ae5a35e4b9ccc2bde994514f2
Eddie <37278660+KN527@users.noreply.github.com>
2019-01-13 22:49:47 +0800

    Update Forum

--- 基础案例/小项目/forum/app.js
+++ 基础案例/小项目/forum/app.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
+var router = require('./router')
 
 var app = express()
 
@@ -9,9 +11,16 @@ app.use('/node_modules', express.static(path.join(__dirname, './node_modules/'))
 app.engine('html', require('express-art-template'))
 app.set('views', path.join(__dirname, './views/'))// 默认的就是views目录
 
-app.get('', function (req, res) {
-	res.send('hello')
-})
+//配置解析表单POST请求体插件
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) 
+// parse application/json
+app.use(bodyParser.json())
+
+// app.get('/', function (req, res) {
+// 	res.render('index.html')
+// })
+app.use(router)
 
 app.listen(3000, function () {
 	console.log('Server running on 3000 ...')
